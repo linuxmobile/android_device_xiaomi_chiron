6207abf2ee00005248367ee2f157a35d109cf68b
WhiterWalt <54509053+WhiterWalt@users.noreply.github.com>
2021-03-09 12:13:53 -0800

    Update and rename aosp_chiron.mk to dot_chiron.mk

--- dot_chiron.mk
+++ dot_chiron.mk
@@ -23,9 +23,9 @@ $(call inherit-product, device/xiaomi/chiron/device.mk)
 $(call inherit-product-if-exists, vendor/apps/GoogleCamera/config.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
-PRODUCT_NAME := aosp_chiron
+PRODUCT_NAME := dot_chiron
 PRODUCT_DEVICE := chiron
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 2
@@ -38,3 +38,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="chiron-user 8.0.0 OPR1.170623.027 V9.5.4.0.ODEMIFA release-keys"
 
 BUILD_FINGERPRINT := Xiaomi/chiron/chiron:8.0.0/OPR1.170623.027/V9.5.4.0.ODEMIFA:user/release-keys
+
+WITH_GAPPS := true
+TARGET_SUPPORTS_BLUR := true
