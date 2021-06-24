a167edebb4a9d8ed99619ba8fe42570abe9b9de5
linux <bdiez19@gmail.com>
2021-06-24 14:01:16 -0300

    Add vendor.qti.hardware.vibrator.service

--- device.mk
+++ device.mk
@@ -40,6 +40,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
+
+PRODUCT_COPY_FILES += \
+      vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # Adding some stuff for descendant
 PRODUCT_BOARD_PLATFORM := msm8998
 PRODUCT_USES_QCOM_HARDWARE=true
